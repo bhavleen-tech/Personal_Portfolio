e69ed19ef831c5b2038b24d318baaf69aece8acb
Bhavleen Kaur <bhavleen0102@gmail.com>
2025-10-14 10:10:06 +0530

    Update Footer.tsx

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; Bhavleen Kaur All rights reserved
             </div>
         </div>
     </div>
